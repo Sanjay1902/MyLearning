a223be496415fe6d4900a892dad892f4a2c557ae
Sanjay1902 <mail2sanjay.social@gmail.com>
2025-09-27 14:23:23 +0530

    Add HelloWorld Java program

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * HelloWorld
+ * This is a simple Java program that prints "Hello, World!" to the console.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
