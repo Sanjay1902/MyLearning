0a58f40e1aa878270513b3060eb774689b698913
Sanjay1902 <mail2sanjay.social@gmail.com>
2025-09-27 14:31:34 +0530

    Update HelloWorld.java with class comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
+        //Print the classic greeting to the console
         System.out.println("Hello, World!");
     }
 }
